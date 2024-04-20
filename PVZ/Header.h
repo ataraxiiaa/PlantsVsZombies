561ebc964253e15186acd646921b9f7f9545a669
ataraxiiaa <i230842@isb.nu.edu.pk>
2024-04-20 21:43:25 +0500

    class test

--- PVZ/Header.h
+++ PVZ/Header.h
@@ -1,6 +1,11 @@
 #pragma once
-
+class Test
+{
+public:
+	Test() = default;
+};
 void testing()
 {
 	std::cout << "Header works" << std::endl;
+	std::cout << "what a gay fuck you are arent you" << endl;
 }
