b94da0e21265937117eaef3b9902722423166565
ataraxiiaa <i230842@isb.nu.edu.pk>
2024-05-12 01:24:20 +0500

    /

--- PVZ/Shooter.cpp
+++ PVZ/Shooter.cpp
@@ -10,34 +10,36 @@ Shooter::Shooter(int totalBullets) // Will change depending on plant
 }
 void Shooter::Fire(sf::RenderWindow& window,Coordinates PlantCoords,int index) // Fire function
 {
-    for (int i = 0; i < totalBullets; ++i) {
-        float bulletDelay = delay * i; // Used to ensure theres a delay if a plant is a repeater
-        // Check if enough time has passed to allow firing
-        if (clock.getElapsedTime().asSeconds() >= bulletDelay && !bullet[i].ShowFire() && this->exists)
-        {
-            // Restart the clock for the next firing interval
-            clock.restart();
-            // Fire the bullet
-            bullet[i].SetFire(true);
-            bullet[i].SetPosition(PlantCoords);
-        }
-
-        // Move the bullet if it's fired
-        if (bullet[i].ShowFire())
-        {
-            // Update bullet position based on its speed
-            Coordinates pos = bullet[i].GetPosition();
-            pos.SetX(pos.GetX() + bullet[i].GetSpeed());
-            bullet[i].SetPosition(pos);
-
-            // Check if the bullet has gone off the screen
-            if (bullet[i].GetPosition().GetX() >= 1150)
+    if (this->exists) {
+        for (int i = 0; i < totalBullets; ++i) {
+            float bulletDelay = delay * i; // Used to ensure theres a delay if a plant is a repeater
+            // Check if enough time has passed to allow firing
+            if (clock.getElapsedTime().asSeconds() >= bulletDelay && !bullet[i].ShowFire() && this->exists)
             {
-                bullet[i].SetFire(false); // Hide the bullet
+                // Restart the clock for the next firing interval
+                clock.restart();
+                // Fire the bullet
+                bullet[i].SetFire(true);
+                bullet[i].SetPosition(PlantCoords);
             }
+
+            // Move the bullet if it's fired
+            if (bullet[i].ShowFire())
+            {
+                // Update bullet position based on its speed
+                Coordinates pos = bullet[i].GetPosition();
+                pos.SetX(pos.GetX() + bullet[i].GetSpeed());
+                bullet[i].SetPosition(pos);
+
+                // Check if the bullet has gone off the screen
+                if (bullet[i].GetPosition().GetX() >= 1150)
+                {
+                    bullet[i].SetFire(false); // Hide the bullet
+                }
+            }
+            // Always draw the bullet (whether fired or not)
+            bullet[i].DrawPea(window);
         }
-        // Always draw the bullet (whether fired or not)
-        bullet[i].DrawPea(window);
     }
 
 }
