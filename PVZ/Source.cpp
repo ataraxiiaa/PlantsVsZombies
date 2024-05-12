852082f4f426d6925881b9a55e690cddf9e9993e
ataraxiiaa <i230842@isb.nu.edu.pk>
2024-05-12 19:35:57 +0500

    ..

--- PVZ/Source.cpp
+++ PVZ/Source.cpp
@@ -1,8 +1,12 @@
 ﻿#include "Game.h" // Including neccessary header files
-
+#include <SFML/Audio.hpp>
 
 int main()
 {
+	sf::SoundBuffer buffer;
+	if (!buffer.loadFromFile("C:/Users/Ali Abdullah/Downloads/music1.mp3"))
+		return -1;
+
 	Game g; // Creating Game object
 	g.Start_Game(); // Game starts
 	return 0;
